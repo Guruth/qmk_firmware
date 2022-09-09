865ae6e25911ae4fd58ba22c461c299e06dce88d
Guruth <1669916+Guruth@users.noreply.github.com>
2022-09-09 09:31:07 +0200

    Added missing \

--- keyboards/lily58/keymaps/lily58l_guruth/keymap.c
+++ keyboards/lily58/keymaps/lily58l_guruth/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      | Caps |-------.    ,-------|      | Left | Down |  UP  | Right|      |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|   |-------|       |      |  =   |      |      |      |
+ * |      |      |      |      |      |      |-------|   |-------|       |      |  =   |      |      |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX,KC_PGDOWN,KC_PGUP, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS,                   XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, KC_EQL , XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, KC_EQL , XXXXXXX, XXXXXXX, KC_BSLS,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 /* ADJUST
--- keyboards/sofle/keymaps/default_guruth/keymap.c
+++ keyboards/sofle/keymaps/default_guruth/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      | Caps |-------.    ,-------|      | Left | Down |  UP  | Right|      |
  * |------+------+------+------+------+------|  MUTE |   |        |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|   |-------|       |      |  =   |      |      |      |
+ * |      |      |      |      |      |      |-------|   |-------|       |      |  =   |      |      |  \   |
  * `-----------------------------------------/      /     \      \-----------------------------------------'
  *            | LAlt | LGUI |LOWER |Space | /      /       \     \  |Enter |RAISE |BackSP | RGUI |
  *            `----------------------------------'           '-----------------------------------'
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , KC_F5  ,  KC_F6 ,                          KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F10, KC_F11 , KC_F12 ,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX,KC_PGDOWN,KC_PGUP, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS,                         XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, XXXXXXX, XXXXXXX, KC_EQL, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, XXXXXXX, XXXXXXX, KC_EQL,  XXXXXXX, XXXXXXX, KC_BSLS,
                     _______, _______, _______, _______, XXXXXXX,       XXXXXXX, _______, _______, _______, _______
 ),
 /* ADJUST
@@ -174,9 +174,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
             tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
         }
     } else if (index == 1) {
         if (clockwise) {
