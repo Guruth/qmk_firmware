43ace62bd4b2f3be74d886c5b2ccfca7a70643f3
Guruth <1669916+Guruth@users.noreply.github.com>
2023-03-03 22:42:59 +0100

    Fixed compilation error

--- keyboards/sofle/keymaps/default_guruth/keymap.c
+++ keyboards/sofle/keymaps/default_guruth/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   KC_ESC,   KC_1,  KC_2,    KC_3,    KC_4,     KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_GRV,
   KC_TAB,   KC_Q,  KC_W,    KC_E,    KC_R,     KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_MINS,
-  KC_LCTRL, KC_A,  KC_S,    KC_D,    KC_F,     KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+  KC_LCTL,  KC_A,  KC_S,    KC_D,    KC_F,     KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,  KC_X,    KC_C,    KC_V,     KC_B,   KC_MUTE,     XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                    KC_LALT, KC_LGUI, LOWER,    KC_SPC, KC_LBRC,     KC_RBRC,  KC_ENT,     RAISE, KC_BSPC, KC_RGUI
 ),
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
    KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , KC_F5  ,  KC_F6 ,                          KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F10, KC_F11 , KC_F12 ,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX,KC_PGDOWN,KC_PGUP, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS,                         XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, XXXXXXX, KC_UNDS, KC_PLUS, XXXXXXX, XXXXXXX, KC_PIPE,
                     _______, _______, _______, _______, XXXXXXX,       XXXXXXX, _______, _______, KC_DEL , _______
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT(
    KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , KC_F5  ,  KC_F6 ,                          KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F10, KC_F11 , KC_F12 ,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX,KC_PGDOWN,KC_PGUP, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS,                         XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       _______, XXXXXXX, XXXXXXX, KC_EQL,  XXXXXXX, XXXXXXX, KC_BSLS,
                     _______, _______, _______, _______, XXXXXXX,       XXXXXXX, _______, _______, _______, _______
@@ -180,7 +180,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_PGDOWN);
+            tap_code(KC_PGDN);
         } else {
             tap_code(KC_PGUP);
         }
